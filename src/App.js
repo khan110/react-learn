83ff2bab24b62163bdb6a644d8fb1c3850edca36
Umair ali khan <umair_kh90@yahoo.com>
2020-06-09 00:59:02 -0500

    Create Media Card with Const

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div>
-      Hello world
-    </div>
-}
 
-export default App;
+
+const Mediacard = (props) => (
+
+  <div>
+            <h2>{props.title}</h2>
+            <p>{props.body} </p>
+  </div>)
+
+
+ export default Mediacard
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Mediacard from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+);*/
+
+ReactDOM.render(
+  <Mediacard title="This is a Media Card" body="This is the description of Media Card" />,
+  document.querySelector("#root")
 );
 
 // If you want your app to work offline and load faster, you can change
